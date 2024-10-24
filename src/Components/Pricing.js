3dc2285947ba60694f35185707e1c1f4950bc3fc
ST017 <jaikishan.k@saralam.tech>
2024-10-24 17:05:34 +0530

    pricing

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,59 +1,117 @@
-import React from 'react';
-import './Pricing.css';
-import MainNavbar from './MainNavbar';
+import React from "react";
+import "./Pricing.css";
+import MainNavbar from "./MainNavbar";
+import righttick from "../Images/check-circle-1.svg";
+import cross from "../Images/check-circle-2.svg";
 
-import Footer from '../Components/Dashboard/Footer';
+import Footer from "../Components/Dashboard/Footer";
 
 const Pricing = () => {
-    document.body.style.background="rgba(234, 239, 255, 1)"
+  document.body.style.background = "rgba(234, 239, 255, 1)";
   return (
-    <>
-    <MainNavbar/>
-      <div className='pricing-cntr'>
-        
-        <div className='price-heading'>
-          <div className='price-heading-main'>
-          <p>Simple, transparent pricing</p>
-          </div>
-          <div className='price-heading-submain'>
-          <p>No contracts. No surprise fees.</p>
-          </div>
-        </div>
-        <div className='price-second-container123'>
-          {/* Free Plan Card */}
-          <div className='card123'>
-            <h2 className="card-title123">Free</h2>
-            <p className="card-subtitle123">Ideal for projects taking their first steps towards building strategic partnerships</p>
-            <p className="card-price123">$0</p>
-            <ul className="card-features123">
-              <li>Limited Send Requests,10 Requests per month for 2 months and 5 per month thereafter</li>
-              <li>Receive Unlimited Partnerships</li>
-              <li>No Trend Data</li>
-              <li>Regular TG support</li>
-              <li>No Newsletter</li>
-            </ul>
-            <button className="card-button123">Get Started</button>
-          </div>
-
-          {/* Paid Plan Card */}
-          <div className='card123' style={{backgroundColor:"rgba(234, 255, 253, 1)"}}>
-            <p className="card-title123">Paid</p>
-            <p className="card-subtitle123">Perfect for projects ready to unlock full potential with unlimited partnership opportunities</p>
-            <p className="card-price123">Pay As You Grow!</p>
-            <p className="card-discount123"><s>$1000</s> $500 for 100 Send Requests</p>
-            <ul className="card-features123">
-              <li>Receive Unlimited partnership requests</li>
-              <li>Trend Data shared via TG</li>
-              <li>Priority Support</li>
-              <li>Bi-Weekly Newsletter with new projects trend data</li>
-            </ul>
-            <button className="card-button123">Contact Sales</button>
-          </div>
-        </div>
-        
+    <div className="pricing">
+      <div>
+        <MainNavbar />
       </div>
-     <div className='footer-ctnr'> <Footer/></div>
-    </>
+      <div className="pricing-cntr">
+        <div className="price-heading">
+          <div className="price-heading-main">
+            <p>Simple, transparent pricing</p>
+          </div>
+          <div className="price-heading-submain">
+            <p>No contracts. No surprise fees.</p>
+          </div>
+        </div>
+        <div className="pricing-cnt1">
+          <div className="pricing-cards-container">
+            <div className="card-p standard-card">
+              <div className="card-title-desc">
+                <div className="card-title-p">Standard</div>
+                <div className="card-description-p">
+                  Ideal for projects taking their first steps towards building
+                  strategic partnerships
+                </div>
+              </div>
+              <div className="card-price">$0</div>
+              <ul className="features-list-p">
+                <li className="feature-item-p">
+                  <img src={righttick} alt="right" /> Limited Send Requests, 10
+                  Requests per month for 2 months and 5 per month thereafter
+                </li>
+                <li className="feature-item-p">
+                  <img src={righttick} alt="right" /> Receive Unlimited
+                  Partnerships
+                </li>
+                <li className="feature-item-p ">
+                  <img src={cross} alt="cross" /> No Trend Data
+                </li>
+                <li className="feature-item-p ">
+                  <img src={righttick} alt="right" /> Regular TG support
+                </li>
+                <li className="feature-item-p ">
+                  <img src={cross} alt="cross" /> No Newsletter
+                </li>
+              </ul>
+              <button className="btn get-started-btn">Get Started</button>
+            </div>
+            <div className="outer-pluscard">
+            <div className="badge save-badge">SAVE 50%</div>
+            <div className="card-p plus-card">
+              <span className="badge popular-badge">Most Popular</span>
+              
+              <div className="card-title-p">Plus ✨</div>
+              <div className="card-description-p">
+                Perfect for projects ready to unlock full potential with
+                unlimited partnership opportunities
+              </div>
+              <div className="card-price">Pay As You Grow!</div>
+
+              <ul className="features-list-p">
+                <li className="feature-item-p">
+                  <img src={righttick} alt="right" />{" "}
+                  <div className="card-subprice">
+                    <strike>$1000</strike> $500 for 100 Send Requests
+                  </div>
+                </li>
+                <li className="feature-item-p">
+                  <img src={righttick} alt="right" /> Receive Unlimited
+                  partnership requests
+                </li>
+                <li className="feature-item-p">
+                  <img src={righttick} alt="right" /> Trend Data shared via TG
+                </li>
+                <li className="feature-item-p">
+                  <img src={righttick} alt="right" /> Priority Support
+                </li>
+                <li className="feature-item-p">
+                  <img src={righttick} alt="right" /> Bi-Weekly Newsletter with
+                  new projects trend data
+                </li>
+              </ul>
+              <button className="btn contact-sales-btn">Contact Sales</button>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div  className="mobile-cnt">
+
+      <div className="pricing-wrapper">
+      <div className="pricing-header">
+        <h1 className="pricing-title">Ready to get started?</h1>
+        <p className="pricing-subtitle">14 days unlimited free trial. No contract or credit card required.</p>
+      </div>
+
+      </div>
+      </div>
+      
+      <div className="footer-ctnr">
+        {" "}
+        <Footer />
+      </div>
+    </div>
   );
 };
 
